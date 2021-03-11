566948a6eb3675c9a0a12851b49f05a7e85108b4
Lannivi <on.a.collision.course@yandex.ru>
2021-03-12 01:45:30 +0400

    feat: solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
+  const ones = ['', 'one','two','three','four','five','six','seven','eight','nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+  const tens =  ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+ if (number === 0) {
+    return 'zero';
+  } 
   
+  if (number > 0 && number < 20) {
+    return ones[number];
+  } 
+
+  let str = number.toString().split('');
+  
+  if (number > 19 && number < 100) {
+    return (tens[str[0]] + ' ' + ones[str[1]]).trim();
+  }
+  
+  if (number > 99) {
+    if (str[1] == '0' && str[2] == '0') {
+      return (ones[str[0]] + ' hundred ').trim();
+    } else
+      return (ones[str[0]] + ' hundred ' + toReadable(Number(str[1] + str[2]))).trim();
+  }
 }
+
+      
+  
